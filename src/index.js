a53af122951c5894d0d6b09053dd88c1ebe4652e
Toby Weston <toby.weston@gmail.com>
2017-02-14 22:54:32 +0000

    placeholders for the RRD charts, the site now looks like the old one (baring the big chart)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Temperature from './Temperature';
 import Band from './Band';
-import { Jumbotron, Grid, Col } from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <div>
+
     <Band />
+
     <Jumbotron>
       <Grid>
         <Col md="3">
@@ -21,6 +23,24 @@ ReactDOM.render(
         </Col>
       </Grid>
     </Jumbotron>
+
+    <Grid>
+      <Row>
+        <Col md="4">
+          <p className="lead">24 Hours</p>
+          <a href="http://localhost:11900/temperature-1-days.png"><img src="http://localhost:11900/temperature-1-days.png" width="320" height="160"/></a>
+        </Col>
+        <Col md="4">
+          <p className="lead">7 Days</p>
+          <a href="http://localhost:11900/temperature-7-days.png"><img src="http://localhost:11900/temperature-7-days.png" width="320" height="160"/></a>
+        </Col>
+        <Col md="4">
+          <p className="lead">1 Month</p>
+          <a href="http://localhost:11900/temperature-30-days.png"><img src="http://localhost:11900/temperature-30-days.png" width="320" height="160"/></a>
+        </Col>
+      </Row>
+    </Grid>
+
   </div>,
 
   document.getElementById('root')
