ce574502519ca46f486839a8f549ec8246a0427c
Toby Weston <toby.weston@gmail.com>
2017-02-14 22:33:24 +0000

    replace bootstrap syntax with react-bootstrap. Does this get me off the hook?

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Temperature from './Temperature';
 import Band from './Band';
+import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <div>
     <Band />
-    <div className="jumbotron">
-      <div className="container">
-        <div className="col-md-3">
-          <p className="lead">Current Temperature</p>
-          <Temperature />
-        </div>
-        <div className="col-md-9">
-          <p className="lead">Last 24 Hours</p>
-          <div className="row" id="chart-img"></div>
-        </div>
-      </div>
-    </div>
+    <Jumbotron>
+      <Grid>
+        <Row>
+          <Col md="3">
+            <p className="lead">Current Temperature</p>
+            <Temperature />
+          </Col>
+          <Col md="9">
+            <p className="lead">Last 24 Hours</p>
+            <div className="row" id="chart-img"></div>
+          </Col>
+        </Row>
+      </Grid>
+    </Jumbotron>
   </div>,
 
   document.getElementById('root')
