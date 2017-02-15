1eb57b1e4af4e5e128b16ccb3fce4d003d603830
Toby Weston <toby.weston@gmail.com>
2017-02-15 20:40:39 +0000

    fix typo

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
 
         <Jumbotron>
           <Grid>
-            <Col md="3">
+            <Col md={3}>
               <Temperatures />
             </Col>
-            <Col md="9">
+            <Col md={9}>
               <p className="lead">Last 24 Hours</p>
               <div className="row" id="chart-img"></div>
             </Col>
@@ -24,15 +24,15 @@ class App extends Component {
 
         <Grid>
           <Row>
-            <Col md="4">
+            <Col md={4}>
               <p className="lead">24 Hours</p>
               <a href="http://localhost:11900/temperature-1-days.png"><img role="presentation" src="http://localhost:11900/temperature-1-days.png" width="320" height="160"/></a>
             </Col>
-            <Col md="4">
+            <Col md={4}>
               <p className="lead">7 Days</p>
               <a href="http://localhost:11900/temperature-7-days.png"><img role="presentation" src="http://localhost:11900/temperature-7-days.png" width="320" height="160"/></a>
             </Col>
-            <Col md="4">
+            <Col md={4}>
               <p className="lead">1 Month</p>
               <a href="http://localhost:11900/temperature-30-days.png"><img role="presentation" src="http://localhost:11900/temperature-30-days.png" width="320" height="160"/></a>
             </Col>
