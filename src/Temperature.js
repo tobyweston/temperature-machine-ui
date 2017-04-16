10e093eb8305d5c3dd0ecd6e5d8fb296c1dfdfbb
Toby Weston <toby.weston@gmail.com>
2017-04-16 20:34:08 +0100

    fixes #12, don't show sensor name if there's only one sensor

--- src/Temperature.js
+++ src/Temperature.js
@@ -5,16 +5,16 @@ class Temperature extends React.Component {
 
   render() {
     const temperatures = this.props.sensors.map(sensor => {
-      return this.renderTemperature(sensor);
+      return this.renderTemperature(sensor, this.props.sensors.length);
     });
     return <div>{ temperatures }</div>
   }
 
-  renderTemperature(sensor) {
+  renderTemperature(sensor, numberOfSensors) {
     const celsius = Math.round(sensor.temperature.celsius * 10) / 10;
     return <div key={sensor.name}>
       <span className="temperature">{ celsius }</span> <span className="scale">°C</span>
-      <p className="source">{ this.props.source } <span className="sensor">({ sensor.name })</span></p>
+      <p className="source">{ this.props.source } { numberOfSensors > 1 && <span className="sensor">({ sensor.name })</span> } </p>
     </div>;
 
   }
