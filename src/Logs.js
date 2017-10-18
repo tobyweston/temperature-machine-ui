067eedbfebc4125c8ac268920816f416ee80bc6a
Toby Weston <toby.weston@gmail.com>
2017-10-18 21:06:14 +0100

    actually load and render the (server) log

--- src/Logs.js
+++ src/Logs.js
@@ -1,16 +1,63 @@
 import React, {Component} from 'react';
 import Footer from './Footer';
+import Spinner from './Spinner';
 import {Link} from 'react-router-dom';
+import axios from "axios";
+import './css/logs.css';
 
 class Logs extends Component {
-
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      log: null
+    }
+  }
+  
   render() {
+
+    let element = null;
+    if (this.state.loading === true)
+      element = <Spinner/>;
+    else if (this.state.error)
+      element = <pre>{ this.state.error.toString() }</pre>;
+    else
+      element = 
+        <div className="log">
+          <pre className="log-data">{ this.state.log }</pre>
+        </div>;
+
+
     return (
         <div>
           <h1>Logs</h1>
+          { element }
           <Footer link={<Link to="/">View the temperatures</Link>}/>
         </div>
     );
+  }
+
+  componentDidMount() {
+    this.fetchLog();
+  }
+  
+  fetchLog() {
+    axios.get('/temperature-machine.log')
+        .then(response => {
+          this.setState({
+            loading: false,
+            error: null,
+            log: response.data
+          })
+        })
+        .catch(error => {
+          this.setState({
+            loading: false,
+            error: error
+          })
+        });
   }
 }
 
