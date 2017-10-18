e474c70b13a6069f37297cf546c9ca2da40b01b7
Toby Weston <toby.weston@gmail.com>
2017-10-18 20:24:52 +0100

    adding react-router-dom to navigate to a new page (to view the logs)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Temperatures from './Temperatures';
 import Band from './Band';
 import RrdCharts from './RrdCharts';
-import { Jumbotron, Grid, Col } from 'react-bootstrap';
+import Logs from './Logs';
+import {Jumbotron, Grid, Col} from 'react-bootstrap';
 import JsonChart from './JsonChart';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './css/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+        <Router>
+          <div>
+            <Band/>
 
-        <Band />
+            <Route exact="true" path="/" render={() => (
+                <div>
+                  <Jumbotron>
+                    <Grid>
+                      <Col md={3}>
+                        <Temperatures/>
+                      </Col>
+                      <Col md={9}>
+                        <JsonChart/>
+                      </Col>
+                    </Grid>
+                  </Jumbotron>
 
-        <Jumbotron>
-          <Grid>
-            <Col md={3}>
-              <Temperatures />
-            </Col>
-            <Col md={9}>
-              <JsonChart />
-            </Col>
-          </Grid>
-        </Jumbotron>
+                  <RrdCharts/>
+                </div>
+            )}/>
 
-        <RrdCharts />
+            <Route path="/logs" component={Logs}/>
 
-        <hr/>
+            <hr/>
+            <footer>
+              <span className="copyright">&copy; 2016-2017 toby weston, <a href="http://temperature-machine.com">temperature-machine.com</a></span>
+              <span className="logs-link"><Link to="logs">View the logs</Link></span>
+            </footer>
 
-        <footer>
-            <span className="copyright">&copy; 2016-2017 toby weston, <a href="http://temperature-machine.com">temperature-machine.com</a></span>
-            <span className="logs-link"><a href="temperature-machine.log">View the logs</a></span>
-        </footer>
-
-      </div>
+          </div>
+        </Router>
     );
   }
 }
---
+++ src/Logs.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './css/app.css';
+
+class Logs extends Component {
+
+  render() {
+    return (
+        <div>
+          <h1>Logs</h1>
+        </div>
+    );
+  }
+}
+
+export default Logs;
