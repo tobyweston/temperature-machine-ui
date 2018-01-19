b0b282a6131c5ff4a57aa132ba972ee98d3fb033
Toby <toby.weston@gmail.com>
2018-01-19 20:48:23 +0000

    show version info in-line rather than it's own section. Also means it's per machine rather than just showing the server info

--- src/Connections.js
+++ src/Connections.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Spinner from './Spinner';
+import Version from './Version';
 import axios from "axios";
 import SidebarMenu from "./SidebarMenu";
 import {Col, Row} from 'react-bootstrap';
@@ -36,17 +37,10 @@ class Connections extends Component {
     else if (this.state.error)
       element = <pre className="error">{this.state.error.toString()}</pre>;
     else
-      element = this.renderBody();
+      element = this.renderConnections();
     return element;
   }
 
-  renderBody() {
-    return <div>
-      { this.renderConnections() }
-      { this.renderVersionInfo() }
-    </div>
-  }
-  
   renderConnections() {
     return <div>
       <Row>
@@ -62,8 +56,9 @@ class Connections extends Component {
           {
             this.state.connections.map((value, index) => {
               return <tr key={index}>
-                <td>{value.host.name}</td>
-                <td>{value.ip.value}</td>
+                <td>{ value.host.name }</td>
+                <td>{ value.ip.value }</td>
+                <td><Version host={ value.ip.value }/></td>
               </tr>;
             })
           }
@@ -72,67 +67,25 @@ class Connections extends Component {
       </div>
     </div>
   }
-
-  renderVersionInfo() {
-    return <div>
-      <Row>
-        <Col md={12}>
-          <h1>Server Info</h1>
-        </Col>
-      </Row>
-
-      <div className='main'>
-        <table>
-          <tbody>
-            <tr>
-              <td>version</td>
-              <td>{this.state.version.version} ({this.state.version.latestSha})</td>
-            </tr>
-            <tr>
-              <td>scala version</td>
-              <td>{this.state.version.scalaVersion}</td>
-            </tr>
-            <tr>
-              <td>sbt version</td>
-              <td>{this.state.version.sbtVersion}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  }
-
-
+  
   componentDidMount() {
     this.fetchConnections();
   }
 
-  getConnections() {
-    return axios.get('/connections')
-  }
-
-  getServerVersion() {
-    return axios.get('/version')
-  }
-
   fetchConnections() {
-    let that = this; // i have no idea why I need this
-    axios.all([this.getConnections(), this.getServerVersion()])
-        .then(axios.spread(function (connections, version) {
-              that.setState({
-                loading: false,
-                error: null,
-                connections: connections.data,
-                version: version.data
-              });
-            }
-        ))
+    axios.get('/connections')
+        .then(response => {
+          this.setState({
+            loading: false,
+            error: null,
+            connections: response.data
+          })
+        })
         .catch(error => {
-          console.log(error)
           this.setState({
             loading: false,
             error: error
-          });
+          })
         });
   }
 
---
+++ src/Version.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import axios from "axios";
+
+// import './css/version.css';
+
+class Version extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      buildInfo: null
+    }
+  }
+
+  render() {
+
+    let element = null;
+    if (this.state.loading)
+      element = null;
+    else if (this.state.error)
+      element = this.renderError();
+    else
+      element = this.renderVersion();
+
+
+    return (
+      <div className="version">
+        { element }
+      </div>
+    );
+  }
+
+  renderError() {
+    return <span className='version-error'>unknown version</span>
+  }
+
+  renderVersion() {
+    return <span>v{ this.state.buildInfo.version } ({ this.state.buildInfo.latestSha })</span>
+  }
+
+  componentDidMount() {
+    this.fetchVersion();
+  }
+
+  fetchVersion() {
+    let url = 'http://' + this.props.host + ':11900/version';
+    axios.get(url)
+        .then(response => {
+          this.setState({
+            loading: false,
+            error: null,
+            buildInfo: response.data
+          })
+        })
+        .catch(error => {
+          this.setState({
+            loading: false,
+            error: error
+          })
+        });
+  }
+
+}
+
+export default Version;
