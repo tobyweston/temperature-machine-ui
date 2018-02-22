9d0c72d854d735fa4b4f200673acdb910e1dc2ae
Toby <toby.weston@gmail.com>
2018-02-22 18:24:16 +0000

    close menu on click / page transition

--- src/SidebarMenu.js
+++ src/SidebarMenu.js
@@ -7,13 +7,24 @@ import 'font-awesome/css/font-awesome.min.css';
 
 class SidebarMenu extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange(state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+  
   render() {
     return (
-      <Menu>
-        <Link to='' onClick={ console.log('click a menu') }><i className="fa fa-thermometer-half"/><span>Temperatures</span></Link>
+      <Menu isOpen={ this.state.menuOpen } onStateChange={ (state) => this.handleStateChange(state) }>
+        <Link onClick={ () => this.closeMenu() } to=''><i className="fa fa-thermometer-half"/><span>Temperatures</span></Link>
         <ToggleShowAveragedSensors showAveragedSensors={ this.props.showAveragedSensors } onToggleShowAveragedSensors={ this.props.onToggleShowAveragedSensors } />
-        <Link to='logs'><i className="fa fa-archive"/><span>Logs</span></Link>
-        <Link to='connections'><i className="fa fa-plug"/><span>Live Connections</span></Link>
+        <Link onClick={ () => this.closeMenu() } to='logs'><i className="fa fa-archive"/><span>Logs</span></Link>
+        <Link onClick={ () => this.closeMenu() } to='connections'><i className="fa fa-plug"/><span>Live Connections</span></Link>
         { this.renderHref('http://temperature-machine.com/docs', '_blank', <i className='fa fa-file-text-o' aria-hidden='true'/>, 'Documentation')}
         { this.renderHref('http://github.com/tobyweston/temperature-machine', '_blank', <i className='fa fa-github-alt' aria-hidden='true'/>, 'Github')}
         { this.renderHref('http://' + location.hostname + ':11900/temperatures.csv', '', <i className='fa fa-cloud-download' aria-hidden='true'/>, 'Export CSV')}
@@ -24,6 +35,11 @@ class SidebarMenu extends Component {
   renderHref(url, target, icon, text) {
     return <a href={ url } target={ target }>{ icon }<span>{ text }</span></a>
   }
+
+  closeMenu () {
+    this.setState({ menuOpen: false })
+  }
+
 }
 
 export default SidebarMenu;
